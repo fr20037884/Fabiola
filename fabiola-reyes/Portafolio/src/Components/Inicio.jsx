3ba804aa441160b49d7b5eaac0be98ec6ec098b2
fr20037884 <137133191+fr20037884@users.noreply.github.com>
2024-04-05 13:38:45 -0300

    Update Inicio.jsx

--- fabiola-reyes/Portafolio/src/Components/Inicio.jsx
+++ fabiola-reyes/Portafolio/src/Components/Inicio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Perfil from './ImagenPortfolio/c:\Users\Note\Desktop\Fabiola\fabiola-reyes\Portafolio\src\Components\ImagenPortafolio\FB_IMG_1680675768934.jpg';
+import Perfil from './ImagensPortfolio/Perfil.png';
 
 import * as S from './Style.jsx';
 
@@ -9,7 +9,7 @@ function Inicio() {
       <S.Perfil src={Perfil} alt="" />
 
       <div>
-        <h2>Olá, me chamo Fabiola Reyes, </h2>
+        <h2>Olá, me chamo Fabiola Reyes </h2>
         <h2>venham navegar pelo o meu portólio.</h2>
       </div>
     </S.ContainerInicio>
