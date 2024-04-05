f424e69e879a07e02e549adc6d5edcdbd80fcde2
fabiola-reyes <reyesfabiola2019@gmail.com>
2024-04-05 09:49:00 -0300

    Create Galeri.jsx

---
+++ fabiola-reyes/Portafolio/src/Components/Galeri.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Galeria() {
+  return (
+    <>
+      <header>
+        <h2>Galeria da Lolo</h2>
+      </header>
+
+      <main>
+        <section>
+         //fotos de cada projeto
+        </section>
+      </main>
+
+      <footer>
+        <h3>Agência de modelos</h3>
+      </footer>
+    </>
+  );
+}
+
+export default Galeria;
