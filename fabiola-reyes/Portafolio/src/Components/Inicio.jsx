cafed64389c8061d6a0dff7a77db9be2032b8237
fr20037884 <137133191+fr20037884@users.noreply.github.com>
2024-04-05 14:33:41 -0300

    Update Inicio.jsx

--- fabiola-reyes/Portafolio/src/Components/Inicio.jsx
+++ fabiola-reyes/Portafolio/src/Components/Inicio.jsx
@@ -9,7 +9,7 @@ function Inicio() {
       <S.Perfil src={Perfil} alt="" />
 
       <div>
-        <h2>Olá, me chamo Fabiola Reyes </h2>
+        <h2>Olá, me chamo Fabiola Reyes, tenho 35 anos, sou professora de educaço comercial, mas geralmente trabahlo em vendas e actualmente estou trabalhando como auxiliar de cozinha e estudando linguages de programaço</h2>
         <h2>venham navegar pelo o meu portólio.</h2>
       </div>
     </S.ContainerInicio>
